8c78dc1c60ee1b210751986e13693e46467a5c33
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-03-20 21:33:08 -0300

    Final da aula 5

--- Aula 4/script.js
+++ Aula 4/script.js
@@ -1,66 +1,140 @@
-
-const turma =
+const turmas = 
+    [
     {
         id: "UC1",
-        nome: "Usabilidade",
-        ementa: "ishiahasisiahais",
-        diasAula: "Quintas e Sextas as 19h"
+        nome: "Usabilidade e Dev Web",
+        ementa: "asdflkj df lsdjfsdlfkj sdlf",
+        diasAula: "5as e 6as às 19h",
+        alunos: ['123', '456', '789']
+    },
+    {
+        id: "UC2",
+        nome: "Sistemas Distribuídos",
+        ementa: "asdflkj df lsdjfsdlfkj sdlf",
+        diasAula: "2as e 3as às 19h",
+        alunos: ['123', '789']
+        
+    }];
+    const alunos = new Map();
+    alunos.set('123',{
+            ra:'123',
+            nome: 'Luana',
+            idade: 20,
+            sexo: 'F'
+        });
+    alunos.set('456',
+        {
+            nome: 'João',
+            idade: 21,
+            sexo: 'M'
+        });
+    alunos.set('789',    
+        {
+            ra: '789',
+            nome: 'Luano',
+            idade: 22,
+            sexo: 'M'
+        });
+    
+    function buscaAluno(ra) {
+        let aluno = alunos.get(ra);
+        if(aluno == undefined){
+            aluno = {ra, 
+                nome: "Aluno não cadastrado",
+                idade: 0
+            }
+        }
+        return aluno;
     }
 
-            /*Não fazer dessa forma*/
-            //var a = 54;
+    function criaTitulo(titulo) {
+        const h1 = document.createElement('h1');
+        h1.innerText = titulo;
+        h1.classList.add('titulo')
+        return h1;
+    }
+    function criaEmenta(ementa) {
+        const div = document.createElement('div');
+        const h2 = document.createElement('h2');
+        h2.innerText = "Ementa";
+        div.appendChild(h2);
+        const p = document.createElement('p');
+        p.innerText = ementa;
+        div.appendChild(p);
+        return div;
+    }
 
-            /*Fazer de preferência assim. Variavel constante que não pode ser modificada*/
-            //const b = 10
+    //Função que lista os alunos
+function criaListaAlunos(alunos) {
+    //Cria uma DIV
+        const div = document.createElement('div');
+    //Cria um H2 
+        const h2 = document.createElement('h2');
+    //Adiciona o titulo Alunos ao H2
+        h2.innerText = "Alunos";
+    //Adiona o H2 dentro da DIV
+        div.appendChild(h2);
+    //Cria uma UL
+        const ul = document.createElement('ul');
+    //Adiciona a UL a DIV
+        div.appendChild(ul);
+    //Cria um For Each onde para cada RA da turma ele executa:
+        alunos.forEach(ra => {
+            //Cria um LI
+            const li = document.createElement('li');
+            //Cria uma Var do tipo aluno que recebe a Função Busca aluno com a ra passada
+            const aluno = buscaAluno(ra);
+            //Com o resultado ele cria um ítem na lista adicionando somente o aluno.NOME
+            li.innerText = aluno.nome;
+            //Devolve para a UL superior o li com a informação do nome criada acima.
+            ul.appendChild(li);
+        });
+        //Retorna a DIV formada.
+        return div;
+    }
+    
+    function cardTurma(turma) {
+        const app = document.getElementById('app');
+        const div = document.createElement('div');
+        div.appendChild( criaTitulo(turma.nome) );
+        div.appendChild(criaEmenta(turma.ementa));
+        div.appendChild( criaListaAlunos(turma.alunos));
+        div.classList.add('card')
+        app.appendChild(div)
+    }
 
-            /*Fazer uma var que pode ser mudada. Ainda assim, elas existem somente dentro do seu escopo ou em escopos menores*/
-            //c=58;
-            //alert(a);
-
-            /*Usar WHILE quando o tamanho do laço não é definido e FOR quando tem um lenght, sempre adicionando +1 ao contador*/
-            /*Usar Switch/case como uma forma de executar por transbordamento. Roda A, B e C. Ou Não roda A mas roda B e C*/
-            
-            let alu1 = {
-                nome: 'Luana',
-                idade: 20,
-                sexo: 'F'
+    function listaSelecaoAlunos(alunos){
+        const ul = document.createElement('ul');
+        alunos.forEach(aluno => {
+            const li = document.createElement('li');
+            ul.appendChild(li);
+            const btn = document.createElement('button');
+            li.appendChild(btn);
+            btn.onclick = () => {
+                turmas[1].alunos.push(aluno.ra);
+                alert(aluno.nome + '' + aluno.ra);
+                refresh();
             }
+            btn.innerText = aluno.nome;
+        })
 
-            const alunos = [
-                'Cristina', alu1, 'Diego', 'Lucio', 'Lukas', 'Luiza', 'Gabriela', 'Larissa', 'Julio', 'Debora'
-            ];
-            function buscarAlunos() {
+        return ul;
+    }
 
-                alunos.push("Gabriel Matos")
-                alunos.sort();
-                const ul = document.getElementById("listaDeAlunos");
-                //ul.innerHTML = "";
-                let itens = '';
-
-                //for(let i = 0; i < alunos.length; i++){
-                //    itens = itens + '<li>'+alunos[i]+'</li>';
-                // };
+    function refresh(){
+        document.getElementById('app').innerHTML = "";
+        turmas.forEach(turma=>cardTurma(turma));
+        //Isto
+        document.getElementById('app').appendChild(listaSelecaoAlunos( Array.from(alunos.values())));
 
 
-                /*Arrow function substitui a chamada "function" por um "=>"*/
-                alunos.forEach((alunos) => {
-                    itens = itens + '<li>'+alunos.nome+'</li>'+'<li>'+alunos.idade+'</li>';
-                });
+        /*E isto fazem a mesma coisa. 
+        const iteradorMapaAlunos = alunos.values();
+        const arrAlunos = Array.from(iteradorMapaAlunos);
+        const lista = listaSelecaoAlunos(arrAlunos);
 
-                ul.innerHTML = itens;
+        const app = document.getElementById('app');
+        app.appendChild(lista);*/
+    }
 
-                /*Visibility: hidden = O elemento acupa espaço mas não aparece. Já o Display: none = Não ocupa espaço e não aparece*/
-                document.getElementById("botaoDeCarregar").classList.add("hidden");
-                document.getElementById("botaoDeLimpar").classList.remove("hidden");  
-            }
-
-            function limpaLista(){
-                const ul = document.getElementById("listaDeAlunos");
-                ul.innerHTML = '';
-                document.getElementById("botaoDeCarregar").classList.remove("hidden");
-                document.getElementById("botaoDeLimpar").classList.add("hidden");
-            }
-            /*Infos sobre documentação W3School = conteúdo leve. MDN =  Conteúdo mais denso*/
-
-
-const 
+    refresh();
