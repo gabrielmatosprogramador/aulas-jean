df1f8b4b37596282942567e5bd15ea2655ad1bcc
jeanpaullopes <jean.lopes@ulife.com.br>
2025-05-22 18:19:06 -0300

    aula passada

---
+++ src/components/CardProduto.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-card class="q-pt-lg q-pl-lg q-pr-lg q-ml-md bg-red-6">
+    <q-card-title class="text-white">
+      <span class="text-h4">{{ produto.nome }}</span>
+    </q-card-title>
+    <q-separator />
+    <q-card-section class="text-body">
+      código: {{ produto.id }}<br />
+      Preço: R$ {{ produto.preco.toFixed(2) }}<br />
+    </q-card-section>
+    <q-card-actions align="right" v-if="mostraBotao">
+      <q-input
+        class="q-mb-md"
+        input-class="text-right"
+        v-model.number="qtd"
+        type="number"
+        min="1"
+        max="99"
+        label="Qtd"
+      />
+      <q-btn
+        outlined
+        @click="addCart()"
+        text-color="white"
+        icon="add_shopping_cart"
+        label="Colocar no Carrinho"
+      />
+    </q-card-actions>
+  </q-card>
+</template>
+<script>
+export default {
+  name: 'CardProduto',
+  props: {
+    produto: Object,
+    mostraBotao: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      qtd: 1,
+    }
+  },
+  emits: ['addCart'],
+  methods: {
+    addCart() {
+      this.$emit('addCart', this.qtd, this.produto)
+    },
+  },
+}
+</script>
+<style></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,49 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-checkbox v-model="logado" />
+    <card-produto
+      @addCart="colocaCarrinho"
+      v-for="prod in produtos"
+      v-bind:key="prod.id"
+      :produto="prod"
+      :mostraBotao="logado"
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
+import CardProduto from 'src/components/CardProduto.vue'
+import axios from 'axios'
 
 export default defineComponent({
-  name: 'IndexPage'
-});
+  name: 'IndexPage',
+  components: { CardProduto },
+  data() {
+    return {
+      produtos: [],
+      logado: false,
+    }
+  },
+  mounted() {
+    const api = axios.create({
+      baseURL: 'http://localhost:3000',
+      timeout: 1000,
+      headers: { 'Content-Type': 'application/json' },
+    })
+    api
+      .get('/produtos')
+      .then((response) => {
+        this.produtos = response.data
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar produtos:', error)
+      })
+  },
+  methods: {
+    colocaCarrinho(qtd, produto) {
+      alert('Produto adicionado ao carrinho: ' + qtd + ' x ' + produto.nome)
+    },
+  },
+})
 </script>
